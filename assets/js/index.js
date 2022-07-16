f3a04f9d4ec8a3073a49396f81882ad59a47db4c
Jhoem Software <45051576+JhoemSoftware@users.noreply.github.com>
2022-07-16 11:30:53 -0500

    Update index.js
    Inserción de comentarios

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,24 +1,36 @@
+// Importación de archivos necesarios
 import {users} from './../data/users.js';
 import {avatars} from './../data/avatars.js';
 
-const tBody = document.getElementById('tBody');
+const tBody = document.getElementById('tBody'); // comunicación con HTML
 
 (()=>{
-    'use strict';
+    'use strict'; // Hacer que el código JS sea estricto en su ejecución
 
     const getAvatar = (documentUser) => {
-        return new Promise((resolve,reject)=>{
-            const image = avatars.find( img => img.id == documentUser )?.url
-            image ? resolve(image) : reject('404 - Image not found')
+        return new Promise((cumpli,noCumpli)=>{ // resolve , reject
+            const image = avatars.find( img => img.id == documentUser )?.url // si la encuentra obtenga la URL (imagen)
+            //image ? resolve(image) : reject('404 - Image not found')
+            if(image){
+                cumpli(image)
+            } else {
+                noCumpli('404 - Image not found')
+            }
         })
     }
     
     const getUser = (identif) => {
         return new Promise((cumplida,noCumplida)=>{
             const user = users.find( u => u.id == identif )
-            user ? cumplida(user) : noCumplida('401 - User not found') 
+            user ? cumplida(user) : noCumplida('401 - User not found') // algo ? entonces algo : otra cosa
         })
     }
+    
+     /*
+    getUser(2)
+        .then(console.log) // CUANDO SE CUMPLE LA PROMESA
+        .catch(console.log) // CUANDO NO SE CUMPLE LA PROMESA
+    */
     
     const callUser = () => {
         for (let i = 1; i < 5; i++) {
@@ -46,6 +58,7 @@ const tBody = document.getElementById('tBody');
                         } )
                 })
                 .catch(console.log)
+                .finally(console.log('Terminé el proceso')) // Para indicar que el proceso finalizó, siempre ingresa sin importar si la promesa se cumple o no
         }
     }
     callUser();
